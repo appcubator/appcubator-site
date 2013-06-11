b2a1c1f5c0b9a8df54be9a56128487b20dee047b
Kedar Amladi <kamladi@gmail.com>
2013-06-11 16:25:33 -0700

    implemented image slider editor

--- appcubator/static/js/application/data/models/SlideModel.js
+++ appcubator/static/js/application/data/models/SlideModel.js
@@ -6,8 +6,8 @@ function() {
   var SlideModel = Backbone.Model.extend({
     initialize: function(bone) {
       if(!bone) bone = {};
-      this.set('image', bone.image || "/static/img/placeholder.png");
-      this.set('text', bone.text || "Sample text. Double click to start editing."); 
+      this.set('image', bone.image || "http://placehold.it/150x150");
+      this.set('text', bone.text || "Sample text. Double click to start editing.");
     }
   });
 
