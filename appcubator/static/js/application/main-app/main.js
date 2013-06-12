35ad34ebc482d992d6db54dd85848793245c9b0a
Ilter Canberk <ilteriscanberk@gmail.com>
2013-06-12 01:52:12 -0700

    Welcome

--- appcubator/static/js/application/main-app/main.js
+++ appcubator/static/js/application/main-app/main.js
@@ -73,52 +73,55 @@ function (AppModel,
           MouseDispatcher,
           Heyoffline) {
 
-  v1State = new Backbone.Model();
-  v1State = new AppModel(appState);
-  v1State.set('pages', new PageCollection(appState.pages||[]));
-  v1State.set('mobilePages', new MobilePageCollection(appState.mobilePages||[]));
+  $(document).ready(function() {
 
-  g_guides = {};
-  keyDispatcher  = new KeyDispatcher();
-  mouseDispatcher  = new MouseDispatcher();
+    v1State = new Backbone.Model();
+    v1State = new AppModel(appState);
+    v1State.set('pages', new PageCollection(appState.pages||[]));
+    v1State.set('mobilePages', new MobilePageCollection(appState.mobilePages||[]));
 
-  v1 = {};
-  v1 = new AppRouter();
-  routeLogger = new RouteLogger({router: v1});
+    g_guides = {};
+    keyDispatcher  = new KeyDispatcher();
+    mouseDispatcher  = new MouseDispatcher();
 
-  Backbone.history.start({pushState: true});
+    v1 = {};
+    v1 = new AppRouter();
+    routeLogger = new RouteLogger({router: v1});
 
-  // handle all click events for routing
-  $(document).on('click', 'a[rel!="external"]', function(e) {
-    var href = e.currentTarget.getAttribute('href');
-    // if internal link, navigate with router
-    if(href.indexOf('/app/'+appId+'/') == 0) {
-      v1.navigate(href, {trigger: true});
-      return false;
-    }
+    Backbone.history.start({pushState: true});
+
+    // handle all click events for routing
+    $(document).on('click', 'a[rel!="external"]', function(e) {
+      var href = e.currentTarget.getAttribute('href');
+      // if internal link, navigate with router
+      if(href.indexOf('/app/'+appId+'/') == 0) {
+        v1.navigate(href, {trigger: true});
+        return false;
+      }
+    });
+
+    // scroll to top button animations
+    var prevScrollPos = 0
+    var $scrollBtn = $('#scrollUp');
+    $(document).on('scroll', function() {
+      var $doc = $(this);
+      var scrollTop = parseInt($doc.scrollTop());
+      var screenHeight = parseInt($doc.height());
+      var isHidden = $scrollBtn.hasClass('hidden');
+      if(scrollTop > (screenHeight/4) && isHidden) {
+        $('#scrollUp').removeClass('hidden');
+      }
+      else if(scrollTop <= (screenHeight/4) && !isHidden) {
+        $('#scrollUp').addClass('hidden');
+      }
+      prevScrollPos = scrollTop;
+    });
+
+    $scrollBtn.on('click', function() {
+      $('html,body').animate({scrollTop:0},100, "linear");
+    });
+
+    // heyoffline config
+    new Heyoffline();
   });
-
-  // scroll to top button animations
-  var prevScrollPos = 0
-  var $scrollBtn = $('#scrollUp');
-  $(document).on('scroll', function() {
-    var $doc = $(this);
-    var scrollTop = parseInt($doc.scrollTop());
-    var screenHeight = parseInt($doc.height());
-    var isHidden = $scrollBtn.hasClass('hidden');
-    if(scrollTop > (screenHeight/4) && isHidden) {
-      $('#scrollUp').removeClass('hidden');
-    }
-    else if(scrollTop <= (screenHeight/4) && !isHidden) {
-      $('#scrollUp').addClass('hidden');
-    }
-    prevScrollPos = scrollTop;
-  });
-
-  $scrollBtn.on('click', function() {
-    $('html,body').animate({scrollTop:0},100, "linear");
-  });
-
-  // heyoffline config
-  new Heyoffline();
 });
