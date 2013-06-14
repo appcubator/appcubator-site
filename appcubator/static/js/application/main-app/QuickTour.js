bcb01d22e4056f06804c8844d4247ea9c1d9aa11
Kedar Amladi <kamladi@gmail.com>
2013-06-13 18:40:18 -0700

    updated walkthrough

--- appcubator/static/js/application/main-app/QuickTour.js
+++ appcubator/static/js/application/main-app/QuickTour.js
@@ -5,13 +5,15 @@ define([
 		//tables pane
 		{
 			target: $('.menu-app-entities'),
-			content: '<h3>Tables</h3><p>Clicking "Tables" will take you to a page to organize your different data entities and relationships</p>',
+			content: '<h3>Tables</h3><p>Clicking "Tables" will take you to a page to organize your different data entities and relationships</p><span class="label label-info">Click the next tab to the right.</span>',
 			my: "top center",
 			at: "bottom center",
-			nextButton: true,
 			closeButton: true,
 			setup: function(tour, options) {
 				$('.menu-app-entities').addClass('active');
+				$('.menu-app-themes').one('click', function(e) {
+					tour.next();
+				});
 			},
 			teardown: function(tour, options) {
 				$('.menu-app-entities').removeClass('active');
@@ -20,13 +22,15 @@ define([
 		//Themes pane
 		{
 			target: $('.menu-app-themes'),
-			content: '<h3>Design & Themes</h3><p>Choose between different web and mobile themes for your site by clicking here</p>',
+			content: '<h3>Design & Themes</h3><p>Choose between different web and mobile themes for your site by clicking here</p><span class="label label-info">Click the next tab to the right.</span>',
 			my: "top center",
 			at: "bottom center",
-			nextButton: true,
 			closeButton: true,
 			setup: function(tour, options) {
 				$('.menu-app-themes').addClass('active');
+				$('.menu-app-pages').one('click', function(e) {
+					tour.next();
+				});
 			},
 			teardown: function(tour, options) {
 				$('.menu-app-themes').removeClass('active');
@@ -36,13 +40,15 @@ define([
 		//Pages pane
 		{
 			target: $('.menu-app-pages'),
-			content: '<h3>Pages</h3><p>Manage your site\'s pages and their layout.</p>',
+			content: '<h3>Pages</h3><p>Manage your site\'s pages and their layout.</p><span class="label label-info">Click the next tab to the right.</span>',
 			my: "top center",
 			at: "bottom center",
-			nextButton: true,
 			closeButton: true,
 			setup: function(tour, options) {
 				$('.menu-app-pages').addClass('active');
+				$('.menu-app-emails').one('click', function(e) {
+					tour.next();
+				});
 			},
 			teardown: function(tour, options) {
 				$('.menu-app-pages').removeClass('active');
@@ -51,13 +57,15 @@ define([
 		//Emails pane
 		{
 			target: $('.menu-app-emails'),
-			content: '<h3>Emails</h3><p>Manage your site\'s emails</p>',
+			content: '<h3>Emails</h3><p>Manage your site\'s emails</p><span class="label label-info">Click the save icon on the right.</span>',
 			my: "top center",
 			at: "bottom center",
-			nextButton: true,
 			closeButton: true,
 			setup: function(tour, options) {
 				$('.menu-app-emails').addClass('active');
+				$('.save-btn').one('click', function(e) {
+					tour.next();
+				});
 			},
 			teardown: function(tour, options) {
 				$('.menu-app-emails').removeClass('active');
@@ -66,33 +74,17 @@ define([
 
 		//Save btn
 		{
-			target: $('.save-btn#save'),
-			content: '<p>Save any changes you make by clicking here</p>',
+			target: $('.save-btn'),
+			content: '<p>Save any changes you make by clicking here</p><span class="label label-info">Click the question mark button below.</span>',
 			my: "right center",
 			at: "left center",
-			nextButton: true,
 			closeButton: true,
 			setup: function(tour, options) {
-				$('').addClass('active');
+				$('.qm-btn').one('click', function(e) {
+					tour.next();
+				});
 			},
 			teardown: function(tour, options) {
-				$('').removeClass('active');
-			}
-		},
-
-		//Deploy btn
-		{
-			target: $('.deploy-pane'),
-			content: '<h3>Deploy</h3><p>Click here to visit your site</p>',
-			my: "right center",
-			at: "left center",
-			nextButton: true,
-			closeButton: true,
-			setup: function(tour, options) {
-				$('').addClass('active');
-			},
-			teardown: function(tour, options) {
-				$('').removeClass('active');
 			}
 		},
 
@@ -102,7 +94,6 @@ define([
 			content: '<p>Click this button at any point for in-depth help</p>',
 			my: "right center",
 			at: "left center",
-			nextButton: true,
 			closeButton: true,
 			setup: function(tour, options) {
 				$('').addClass('active');
