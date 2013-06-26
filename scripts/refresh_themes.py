a91d133dd3c388f6a9911c2e4e529ce6a43831d1
Karan Sikka <kssworld93@gmail.com>
2013-06-26 15:02:59 -0700

    setings to themes

--- scripts/refresh_themes.py
+++ scripts/refresh_themes.py
@@ -1,7 +1,12 @@
 from appcubator.models import UITheme, load_initial_themes
+import sys
+import os
 
 if __name__ == "__main__":
 
+    if len(sys.argv) > 1:
+        os.environ['DJANGO_SETTINGS_MODULE'] = sys.argv[1]
+    print "deleting themes"
     for t in UITheme.objects.all():
         t.delete()
     load_initial_themes()
