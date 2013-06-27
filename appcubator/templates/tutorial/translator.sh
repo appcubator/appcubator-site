7772f9d87443b8bd414f1ed663e4b662eb92ea5b
Ilter Canberk <ilteriscanberk@gmail.com>
2013-06-27 13:30:06 -0700

    Shell scripting

--- appcubator/templates/tutorial/translator.sh
+++ appcubator/templates/tutorial/translator.sh
@@ -1,6 +1,11 @@
+OLD="http://appcubator.com/static"
+NEW="{{STATIC_URL}}"
+
 for f in md/*.md; 
 do 
     echo "Processing $f file..";
     fname=`basename $f` 
-    markdown2 $f > html/"${fname%.md}.html"
+    hname=html/"${fname%.md}.html"
+    markdown2 $f > $hname
+    sed -ie "s,$OLD,$NEW,g" "$hname"
 done
