396e51dec9dce2e61e7edb0cd1ddec606cbad2ad
Ilter Canberk <ilteriscanberk@gmail.com>
2013-07-05 13:38:39 -0700

    Back to the future

--- appcubator/static/js/application/data/models/ContentModel.js
+++ appcubator/static/js/application/data/models/ContentModel.js
@@ -1,4 +1,23 @@
 define(['backbone'], function(Backbone) {
-  var ContentModel = Backbone.Model.extend({ });
+  var ContentModel = Backbone.Model.extend({
+
+    initialize: function(bone) {
+      if(bone.src && util.isInternalData(bone.src)) {
+        console.log("YO");
+        this.set('src_content', _.clone(bone.src));
+        this.set('src', "/static/img/placeholder.png");
+      }
+    },
+
+    toJSON: function() {
+      var json = _.clone(this.attributes);
+      if(json.content_attribs && json.content_attribs.src_content) {
+        console.log("HEY");
+        json.content_attribs.src = json.content_attribs.src_content;
+      }
+      return json;
+    }
+
+  });
   return ContentModel;
 });
