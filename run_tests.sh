1f37acfc1155edda6baa87c8c3cb12ed5f0d2ed9
Nikhil Khadke <nikhil.khadke@gmail.com>
2013-07-06 19:56:17 -0700

    x11 fix

--- run_tests.sh
+++ run_tests.sh
@@ -2,10 +2,10 @@
 # run_tests.sh
 host=$1
 echo "Running tests on $host"
-phantomjs appcubator/static/js/test/lib/run_jasmine_test.coffee $host/test/editor/
-phantomjs appcubator/static/js/test/lib/run_jasmine_test.coffee $host/test/router/
-phantomjs appcubator/static/js/test/lib/run_jasmine_test.coffee $host/test/data/
-phantomjs appcubator/static/js/test/lib/run_jasmine_test.coffee $host/test/formeditor/
-phantomjs appcubator/static/js/test/lib/run_jasmine_test.coffee $host/test/tables/
-phantomjs appcubator/static/js/test/lib/run_jasmine_test.coffee $host/test/thirdparty/
+xvfb-run phantomjs appcubator/static/js/test/lib/run_jasmine_test.coffee $host/test/editor/
+xvfb-run phantomjs appcubator/static/js/test/lib/run_jasmine_test.coffee $host/test/router/
+xvfb-run phantomjs appcubator/static/js/test/lib/run_jasmine_test.coffee $host/test/data/
+xvfb-run phantomjs appcubator/static/js/test/lib/run_jasmine_test.coffee $host/test/formeditor/
+xvfb-run phantomjs appcubator/static/js/test/lib/run_jasmine_test.coffee $host/test/tables/
+xvfb-run phantomjs appcubator/static/js/test/lib/run_jasmine_test.coffee $host/test/thirdparty/
 
