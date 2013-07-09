86484dd86f53965defd37f5072704095cb595d58
Ilter Canberk <ilteriscanberk@gmail.com>
2013-07-09 12:53:35 -0700

    Documentation

--- appcubator/urls.py
+++ appcubator/urls.py
@@ -84,7 +84,8 @@ urlpatterns += patterns('appcubator.views',
     # special json editor route
     url(r'^app/(\d+)/editor/\d+/debug/$', 'app_json_editor'), # this serves all the app pages
 
-    url(r'^tutorial/([^/]+)/$', 'tutorial_page'),
+    url(r'^documentation/$', 'documentation_page', {"page_name": "intro"}),
+    url(r'^documentation/([^/]+)/$', 'documentation_page'),
 
     # the rest
     url(r'^app/(\d+)/', 'app_page'), # this serves all the app pages
