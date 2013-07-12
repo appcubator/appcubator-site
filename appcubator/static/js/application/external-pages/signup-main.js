d032a611d050d96602dbccd6be733b49e7f769f9
Ilter Canberk <ilteriscanberk@gmail.com>
2013-07-12 12:37:55 -0700

    Prevent negative vals

--- appcubator/static/js/application/external-pages/signup-main.js
+++ appcubator/static/js/application/external-pages/signup-main.js
@@ -38,30 +38,30 @@ require([
       });
 
       $('form#signup').submit(function(e) {
-        console.log($(e.target).serialize());
-        var self = this;
-        var ajax_info = {
-         type: 'POST',
-         url: '/signup/',
-         data: $(e.target).serialize(),
-         success: function(data, statusStr, xhr) {
-          if (typeof(data.redirect_to) !== 'undefined') {
-            location.href = data.redirect_to;
-          } else {
-            _.each(data, function(val, key, ind) {
-              if(key==='__all__') {
-                $(self).find('.form-error.field-all').html(val.join('<br />'));
-              } else {
-                $(self).find('.form-error.field-name-'+key).html(val.join('<br />'));
-              }
-            });
+          console.log($(e.target).serialize());
+          var self = this;
+          var ajax_info = {
+           type: 'POST',
+           url: '/signup/',
+           data: $(e.target).serialize(),
+           success: function(data, statusStr, xhr) {
+            if (typeof(data.redirect_to) !== 'undefined') {
+              location.href = data.redirect_to;
+            } else {
+              _.each(data, function(val, key, ind) {
+                if(key==='__all__') {
+                  $(self).find('.form-error.field-all').html(val.join('<br />'));
+                } else {
+                  $(self).find('.form-error.field-name-'+key).html(val.join('<br />'));
+                }
+              });
+            }
           }
-        }
-      };
-      $.ajax(ajax_info);
-      $(self).find('.form-error').html("");
-      return false;
-    });
+        };
+        $.ajax(ajax_info);
+        $(self).find('.form-error').html("");
+        return false;
+      });
 
     });
   };
