33eeaa59ac548ccec498889a7080e44642dddfab
Ilter Canberk <ilteriscanberk@gmail.com>
2013-07-12 12:07:04 -0700

    admin panel ratchet

--- appcubator/static/js/application/external-pages/hn-main.js
+++ appcubator/static/js/application/external-pages/hn-main.js
@@ -137,6 +137,32 @@ function() {
     $('input[type=checkbox]').prettyCheckable();
     $('input[type=radio]').prettyCheckable();
 
+    $('form#signup').submit(function(e) {
+          console.log($(e.target).serialize());
+          var self = this;
+          var ajax_info = {
+           type: 'POST',
+           url: '/signup/',
+           data: $(e.target).serialize(),
+           success: function(data, statusStr, xhr) {
+            if (typeof(data.redirect_to) !== 'undefined') {
+              location.href = data.redirect_to;
+            } else {
+              _.each(data, function(val, key, ind) {
+                if(key==='__all__') {
+                  $(self).find('.form-error.field-all').html(val.join('<br />'));
+                } else {
+                  $(self).find('.form-error.field-name-'+key).html(val.join('<br />'));
+                }
+              });
+            }
+          }
+        };
+        $.ajax(ajax_info);
+        $(self).find('.form-error').html("");
+        return false;
+    });
+
     $('#sign-up-form').on('submit', function(e) {
       e.preventDefault();
 
