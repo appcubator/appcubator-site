c20003610b3dcee5921f7cdfd0edb86892c771c3
Kedar Amladi <kamladi@gmail.com>
2013-07-15 11:11:45 -0700

    admin pages only visible to superusers

--- appcubator/admin_views.py
+++ appcubator/admin_views.py
@@ -1,21 +1,15 @@
 from django.http import HttpResponse
-from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_GET, require_POST
+from django.contrib.auth.decorators import user_passes_test
 from django.utils import simplejson
 from django.shortcuts import redirect, render, render_to_response, get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
-from django.core.urlresolvers import reverse
 
 from models import App, StaticFile, UITheme, ApiKeyUses, ApiKeyCounts, AppstateSnapshot, LogAnything, Customer, ExtraUserData
 from email.sendgrid_email import send_email
 from models import DomainRegistration
 from models import get_default_uie_state, get_default_mobile_uie_state
 from models import get_default_app_state, get_default_theme_state
-
-import app_builder.analyzer as analyzer
-from app_builder.analyzer import App as AnalyzedApp
-from app_builder.utils import get_xl_data, add_xl_data, get_model_data
 
 import requests
 import traceback
@@ -25,24 +19,24 @@ import subprocess
 import os
 from datetime import datetime
 
-@login_required
+@user_passes_test(lambda u: u.is_superuser)
 def admin_home(request):
     page_context = {}
     return render(request, 'admin/home.html', page_context)
 
-@login_required
+@user_passes_test(lambda u: u.is_superuser)
 def admin_customers(request):
     page_context = {}
     page_context["customers"] = Customer.objects.all()
     return render(request, 'admin/customers.html', page_context)
 
-@login_required
+@user_passes_test(lambda u: u.is_superuser)
 def admin_users(request):
     page_context = {}
     page_context["users"] = ExtraUserData.objects.all()
     return render(request, 'admin/users.html', page_context)
 
-@login_required
+@user_passes_test(lambda u: u.is_superuser)
 def admin_user(request, user_id):
     user_id = long(user_id)
     user = get_object_or_404(ExtraUserData, id=user_id)
@@ -52,13 +46,13 @@ def admin_user(request, user_id):
     page_context["userlogs"] = logs
     return render(request, 'admin/user.html', page_context)
 
-@login_required
+@user_passes_test(lambda u: u.is_superuser)
 def admin_apps(request):
     page_context = {}
     page_context["apps"] = App.objects.all()
     return render(request, 'admin/apps.html', page_context)
 
-@login_required
+@user_passes_test(lambda u: u.is_superuser)
 def admin_app(request, app_id):
     app_id = long(app_id)
     app = get_object_or_404(App, id=app_id, owner=request.user)
@@ -66,7 +60,7 @@ def admin_app(request, app_id):
     page_context["app"] = app
     return render(request, 'admin/app.html', page_context)
 
-@login_required
+@user_passes_test(lambda u: u.is_superuser)
 def admin_feedback(request):
     page_context = {}
     page_context["feedback"] = LogAnything.objects.filter(name='posted feedback')
