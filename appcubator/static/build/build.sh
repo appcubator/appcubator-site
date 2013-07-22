b96e950e38dc9a6ba7552b6ae999240362e90f1a
Kedar Amladi <kamladi@gmail.com>
2013-07-22 14:03:43 -0700

    moar build script edits

--- appcubator/static/build/build.sh
+++ appcubator/static/build/build.sh
@@ -3,5 +3,3 @@ echo "Copied main.js to main.js.backup"
 lessc -x --yui-compress ./static/css/style.less ./static/css/style.css
 r.js -o ./static/build/app.build.js
 cp ./static/js/application/main-app/main.js.backup ./static/js/application/main-app/main.js
-cd ./dist_static
-rm -rf build
