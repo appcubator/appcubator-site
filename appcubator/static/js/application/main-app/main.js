03fbab9df6d544daf71a2a11dee9c9caa4fefc93
Ilter Canberk <ilteriscanberk@gmail.com>
2013-07-23 16:24:01 -0700

    Simple and in depth

--- appcubator/static/js/application/main-app/main.js
+++ appcubator/static/js/application/main-app/main.js
@@ -116,6 +116,17 @@ function (AppModel,
     Backbone.history.start({pushState: true});
 
     if(v1State.has('walkthrough')) {
+      require(['app/TwitterTour'], function(QuickTour) {
+        if(!QuickTour.currentStep) return;
+        var url = QuickTour.currentStep.url;
+        v1.navigate('app/'+appId+url, {trigger: true});
+        setTimeout(function() {
+          QuickTour.start();
+        }, 1000);
+      });
+    }
+
+    if(v1State.has('simpleWalkthrough')) {
       require(['app/SimpleTwitterTour'], function(QuickTour) {
         if(!QuickTour.currentStep) return;
         var url = QuickTour.currentStep.url;
