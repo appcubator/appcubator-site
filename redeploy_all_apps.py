69f98f004bca77c9b1705eba47c0dbe80247580f
v1 Factory <founders@v1factory.com>
2013-07-25 20:48:23 +0000

    ye

--- redeploy_all_apps.py
+++ redeploy_all_apps.py
@@ -5,7 +5,8 @@ fail_summaries = []
 def summary(app):
     return (app.owner.id, app.owner.email, app.id, app.deployment_id, )
 
-for a in App.objects.exclude(owner__email="!@TEST_USER@!@gmail.com"):
+for a in App.objects.exclude(owner__email="!@TEST__USER@!@gmail.com"):
+    #a = App.objects.get(pk=app_id)
     s = summary(a)
     try:
         a.deploy()
