bc92834416b835283e42465877443b6bc3df8a5b
Nikhil Khadke <nikhil.khadke@gmail.com>
2013-07-24 17:42:21 -0700

    Added template emailer

--- appcubator/email/sendgrid_email.py
+++ appcubator/email/sendgrid_email.py
@@ -1,6 +1,8 @@
 import smtplib
+
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from django.template.loader import render_to_string
 
 def send_email(from_email, to_email, subject, text, html):
   msg = MIMEMultipart('alternative')
@@ -17,3 +19,7 @@ def send_email(from_email, to_email, subject, text, html):
   s.login('maverickn', 'obscurepassword321')
   s.sendmail(from_email, to_email, msg.as_string())
   s.quit()
+
+def send_template_email(from_email, to_email, subject, text, template_file, template_context):
+  html = "<pre>%s</pre>" % render_to_string(template_file, template_context)
+  send_email(from_email, to_email, subject, text, html)
