cea6c69c3aff0e381796ee3b1ba5900dfe3ce343
Ilter Canberk <ilteriscanberk@gmail.com>
2013-08-04 02:13:58 -0400

    new homepage

--- appcubator/base_views.py
+++ appcubator/base_views.py
@@ -78,15 +78,7 @@ def homepage(request):
     page_context = {}
     page_context["title"] = "Homepage"
 
-    return render(request, 'website-home.html', page_context)
-
-@require_GET
-def homepagenew(request):
-    page_context = {}
-    page_context["title"] = "Homepage"
-
     return render(request, 'website-home-new.html', page_context)
-
 
 @require_GET
 def showhnpage(request):
