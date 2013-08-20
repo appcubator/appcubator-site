7950505f8c854e04cdf0362a660023b0a9498c40
v1 Factory <founders@v1factory.com>
2013-08-20 22:20:55 +0000

    redeploy script tracks successes and failures

--- redeploy_all_apps.py
+++ redeploy_all_apps.py
@@ -2,10 +2,12 @@ from appcubator.models import App
 import traceback
 
 fail_summaries = []
+pass_summaries = []
 def summary(app):
     return (app.owner.id, app.owner.email, app.id, app.deployment_id, )
 
 for a in App.objects.exclude(owner__email="!@TEST__USER@!@gmail.com"):
+#for a in App.objects.all()[1:10]:
     s = summary(a)
     try:
         a.deploy()
@@ -14,9 +16,14 @@ for a in App.objects.exclude(owner__email="!@TEST__USER@!@gmail.com"):
         print "Fail: %r" % (s,)
         print traceback.format_exc()
     else:
+        pass_summaries.append([x for x in s])
         print "Success: %r" % (s,)
 
-with open("redeploy.log", "w") as rl:
+with open("failed_rd.log", "w") as rl:
     log_text = "\n".join([ "\t".join([repr(d) for d in tupl]) for tupl in fail_summaries ])
+    rl.write(log_text) # each line
+
+with open("passed_rd.log", "w") as rl:
+    log_text = "\n".join([ "\t".join([repr(d) for d in tupl]) for tupl in pass_summaries ])
     rl.write(log_text) # each line
 
