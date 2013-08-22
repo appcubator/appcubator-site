8d8636acde6c556323f81cc30d09c5b5996d1a7d
Ilter Canberk <ilteriscanberk@gmail.com>
2013-08-22 14:58:02 -0700

    fix

--- appcubator/static/js/application/main-app/editor/WidgetView.js
+++ appcubator/static/js/application/main-app/editor/WidgetView.js
@@ -280,7 +280,7 @@ define([ 'backbone', 'mixins/BackboneUI'],  function() {
             this.editMode = false;
             this.$el.removeClass('textediting');
             var el = $(this.el.firstChild);
-            val = el[0].innerText;
+            var val = el[0].innerText;
             this.model.get('data').set('content', val);
             el.attr('contenteditable', 'false');
             keyDispatcher.textEditing = false;
