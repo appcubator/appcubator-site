ed7385f32c2133ef6015e91e90521304b023d2ff
Ilter Canberk <ilteriscanberk@gmail.com>
2013-08-23 13:24:28 -0700

    Twitter button doesn't disappear

--- appcubator/static/js/application/main-app/ShareModalView.js
+++ appcubator/static/js/application/main-app/ShareModalView.js
@@ -24,12 +24,19 @@ function() {
       var template = util.getHTML('share-panel');
       this.el.innerHTML = _.template(template, {site_url : appUrl});
 
-      !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+      this.g_js = {};
+      var self = this;
+      !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s); self.g_js = js; js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
       return this;
     },
 
     logDownload: function() {
       util.log_to_server('code downloaded', {}, appId);
+    },
+
+    close: function() {
+      this.g_js.parentNode.removeChild(this.g_js);
+      ShareModalView.__super__.close.call(this);
     }
   });
 
