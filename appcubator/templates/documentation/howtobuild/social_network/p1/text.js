daa5749b20973ac5594080d4a5a73a248945acc7
Karan Sikka <karanssikka@gmail.com>
2013-08-29 19:12:13 -0400

    the ball is rolling

--- appcubator/templates/documentation/howtobuild/social_network/p1/text.js
+++ appcubator/templates/documentation/howtobuild/social_network/p1/text.js
@@ -1,26 +1,24 @@
 var s1 = {
-    beforeHtml: 'Login, and on the top right dropdown menu, click Create a New App to get started.' +
+    beforeHtml: 'Login, and on the top right dropdown menu, click Create a New App to get started. ' +
           'We\'ll call this one Bookface.',
-    afterHtml: 'Now you should be at the Overview page. This is like the control panel for your web app.' +
-          'The top navigation bar will have a link to the Pages page. Go there, and start editing the Homepage.',
+    afterHtml: 'After that, you should be at the Overview page. This is the control panel for your web app. ' +
+          'The top navigation bar will have a link to the Pages page. Go there, and start editing the Homepage. ',
 };
 var s2 = {
-    beforeHtml: '' +
-          '',
-    afterHtml: '' +
-          '',
+    beforeHtml: 'When you edit a blank page, a popup window will open asking you if you want to start with a page template. ' +
+          'For this tutorial, we\'ll start with a blank page. ' +
+          'The elements on the right hand side are draggable onto the editor. Drag some elements onto the editor as shown below:',
+    afterHtml: ''
 };
 var s3 = {
-    beforeHtml: '' +
-          '',
-    afterHtml: '' +
-          '',
+    beforeHtml: 'Customize the text by double clicking on an element. ' +
+          'Click "Pick Style" on the widget editor and pick one of the available styles.',
+    afterHtml: 'Styles are editable in the Theme editor, you can read about that in the How to Design Themes tutorial.'
 };
 var s4 = {
-    beforeHtml: '' +
-          '',
-    afterHtml: '' +
-          '',
+    beforeHtml: 'Click on the image element you dragged on the page, then change the image source. ',
+    afterHtml: 'When you click Upload Image, you\'ll see this window, where you can upload images from a ' +
+          'number of sources. In this example, we\'ll just search the web to find something simple.',
 };
 var s5 = {
     beforeHtml: '' +
