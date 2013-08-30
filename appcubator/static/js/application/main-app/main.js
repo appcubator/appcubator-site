d39bca67eaf65833941fb99199f2be770737986d
Ilter Canberk <ilteriscanberk@gmail.com>
2013-08-29 22:26:48 -0400

    attemp to switch react to cdn

--- appcubator/static/js/application/main-app/main.js
+++ appcubator/static/js/application/main-app/main.js
@@ -5,7 +5,7 @@ require.config({
     "jquery.hotkeys"  : "../../libs/jquery/jquery.hotkeys",
     "underscore"      : "../../libs/underscore-amd/underscore",
     "backbone"        : "../../libs/backbone-amd/backbone",
-    "react"           : "../../libs/react",
+    "react"           : "https://fb.me/react-0.4.1.min",
     "heyoffline"      : "../../libs/heyoffline",
     "util"            : "../../libs/util/util",
     "util.filepicker" : "../../libs/util/util.filepicker",
