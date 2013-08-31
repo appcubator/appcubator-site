54314f069faaf6f7036b41b05cc5446b14ae73e5
Karan Sikka <karanssikka@gmail.com>
2013-08-30 20:13:42 -0400

    wall posts

--- appcubator/templates/documentation/howtobuild/social_network/p4/text.js
+++ appcubator/templates/documentation/howtobuild/social_network/p4/text.js
@@ -1,46 +1,96 @@
 var s1 = {
+    short: 'Make a Table for Wall posts',
     beforeHtml: 'Login, and on the top right dropdown menu, click Create a New App to get started.' +
           'We\'ll call this one Bookface.',
     afterHtml: 'Now you should be at the Overview page. This is like the control panel for your web app.' +
           'The top navigation bar will have a link to the Pages page. Go there, and start editing the Homepage.',
 };
 var s2 = {
+    short: 'Add a relation to associate Wall posts with Users who write them',
     beforeHtml: '' +
           '',
     afterHtml: '' +
           '',
 };
 var s3 = {
+    short: 'Add a relation to associate Wall posts with User\'s wall',
     beforeHtml: '' +
           '',
     afterHtml: '' +
           '',
 };
 var s4 = {
+    short: 'Drag a Wall post list onto the Profile page',
     beforeHtml: '' +
           '',
     afterHtml: '' +
           '',
 };
 var s5 = {
+    short: 'Show only the wall posts that belong on the wall',
     beforeHtml: '' +
           '',
     afterHtml: '' +
           '',
 };
 var s6 = {
+    short: 'Drag a Create Wall post form onto the Profile page',
     beforeHtml: '' +
           '',
     afterHtml: '' +
           '',
 };
 var s7 = {
+    short: 'Customize the form and configure the relationships',
     beforeHtml: '' +
           '',
     afterHtml: '' +
           '',
 };
-var s8 = {
+var s11 = {
+    short: 'Create a new page for the News feed',
+    beforeHtml: '' +
+          '',
+    afterHtml: '' +
+          '',
+};
+var s13 = {
+    short: 'Drag a Wall post list and design the rows',
+    beforeHtml: '' +
+          '',
+    afterHtml: '' +
+          '',
+};
+var s15 = {
+    short: 'Let\'s try it out using a test account!',
+    beforeHtml: '' +
+          '',
+    afterHtml: '' +
+          '',
+};
+var s16 = {
+    short: 'Go to Isaac Newton\'s profile',
+    beforeHtml: '' +
+          '',
+    afterHtml: '' +
+          '',
+};
+var s18 = {
+    short: 'Post on his wall',
+    beforeHtml: '' +
+          '',
+    afterHtml: '' +
+          '',
+};
+var s19 = {
+    short: 'It shows up on his profile',
+    beforeHtml: '' +
+          '',
+    afterHtml: '' +
+          '',
+};
+var s20 = {
+    short: 'as well as on the newsfeed!',
     beforeHtml: '' +
           '',
     afterHtml: '' +
