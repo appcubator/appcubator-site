377424097ac7891b6ce5fe4ad1b9f12c6bfd2046
Ilter Canberk <ilteriscanberk@gmail.com>
2013-09-07 09:20:56 -0700

    quickstart working

--- appcubator/static/js/application/website/ExternalEditorView.js
+++ appcubator/static/js/application/website/ExternalEditorView.js
@@ -6,6 +6,19 @@ function( EditorView) {
   var ExternalEditorView = EditorView.extend({
 
 
+    render: function() {
+        ExternalEditorView.__super__.render.call(this);
+        
+        
+        require(['./QuickStartTour'], function(QuickTour) {
+                if(!QuickTour.currentStep) return;
+                var url = QuickTour.currentStep.url;
+                QuickTour.start();
+        });
+
+        return this;
+    }
+
   });
 
   return ExternalEditorView;
