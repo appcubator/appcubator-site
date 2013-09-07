ebc39bf1e9abea2ae023f226d1d1034586f2f096
v1 Factory <founders@v1factory.com>
2013-09-07 17:51:04 +0000

    fixed build script

--- appcubator/static/build/build.sh
+++ appcubator/static/build/build.sh
@@ -1,6 +1,7 @@
 cp ./static/js/application/main-app/main.js ./static/js/application/main-app/main.js.backup
 echo "Copied main.js to main.js.backup"
 lessc --verbose --rootpath=/static/css/app/ -x --yui-compress --ru --line-numbers=mediaquery ./static/css/app/style.less ./static/css/app/style.css
+lessc --verbose --rootpath=/static/css/app/ -x --yui-compress --ru --line-numbers=mediaquery ./static/css/app/editor/editor.less ./static/css/app/editor/editor.css
 echo "Compiled app/style.less to app.css"
 lessc --verbose --rootpath=/static/css/ -x --yui-compress --ru --line-numbers=mediaquery ./static/css/documentation.less ./static/css/documentation.css
 echo "Compiled documentation.less to documentation.css"
