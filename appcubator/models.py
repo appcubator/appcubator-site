0244225ee6935c8d48421fe5eaef477c4dcb20ab
Karan Sikka <karanssikka@gmail.com>
2013-09-07 22:50:41 -0700

    lol

--- appcubator/models.py
+++ appcubator/models.py
@@ -221,9 +221,9 @@ class TempDeployment(RandomPrimaryIdModel):
     @classmethod
     def create(cls):
         i = cls()
-        i.s = str(random.randint(10000000,90000000))
+        i.subdomain = str(random.randint(10000000,90000000))
         i.save()
-        i.s = "temp-%s" % i.id#cls.get_rand_uniq_subdomain()
+        i.subdomain = "temp-%s" % i.id#cls.get_rand_uniq_subdomain()
         i.save()
         return i
 
