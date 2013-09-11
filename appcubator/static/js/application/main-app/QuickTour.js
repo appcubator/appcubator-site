103ca76f1aeec9c163d93cbe6be870e24ef856c4
Ilter Canberk <ilteriscanberk@gmail.com>
2013-09-11 13:40:35 -0400

    one csrf

--- appcubator/static/js/application/main-app/QuickTour.js
+++ appcubator/static/js/application/main-app/QuickTour.js
@@ -4,6 +4,24 @@ define([
 ],
 function() {
 	var steps = [
+		//Pages pane
+		{
+			target: $('.menu-app-pages'),
+			title   : 'Pages',
+			content : 'Click here to manage your site\'s pages and their layout.',
+			loc: "top center, bottom center",
+			closeButton: true,
+			highlightTarget: true,
+			setup: function(tour, options) {
+				$('.menu-app-pages').addClass('active');
+				$('.menu-app-pages').one('click', function(e) {
+					tour.next();
+				});
+			},
+			teardown: function(tour, options) {
+				$('.menu-app-pages').removeClass('active');
+			}
+		},
 		//tables pane
 		{
 			title  : 'Tables',
@@ -11,6 +29,7 @@ function() {
 			target: $('.menu-app-entities'),
 			loc: "top center, bottom center",
 			closeButton: true,
+			highlightTarget: true,
 			setup: function(tour, options) {
 				$('.menu-app-entities').addClass('active');
 				$('.menu-app-entities').one('click', function(e) {
@@ -28,6 +47,7 @@ function() {
 			content : 'Click here to choose between different web and mobile themes for your site by clicking here.',
 			loc: "top center, bottom center",
 			closeButton: true,
+			highlightTarget: true,
 			setup: function(tour, options) {
 				$('.menu-app-themes').addClass('active');
 				$('.menu-app-themes').one('click', function(e) {
@@ -38,51 +58,32 @@ function() {
 				$('.menu-app-themes').removeClass('active');
 			}
 		},
-
-		//Pages pane
-		{
-			target: $('.menu-app-pages'),
-			title   : 'Pages',
-			content : 'Click here to manage your site\'s pages and their layout.',
-			loc: "top center, bottom center",
-			closeButton: true,
-			setup: function(tour, options) {
-				$('.menu-app-pages').addClass('active');
-				$('.menu-app-pages').one('click', function(e) {
-					tour.next();
-				});
-			},
-			teardown: function(tour, options) {
-				$('.menu-app-pages').removeClass('active');
-			}
-		},
-		//Emails pane
-		// {
-		// 	target: $('.menu-app-emails'),
-		// 	content: '<h3>Emails</h3><p>Click here to manage your site\'s emails</p>',
-		// 	my: "top center",
-		// 	at: "bottom center",
-		// 	closeButton: true,
-		// 	setup: function(tour, options) {
-		// 		$('.menu-app-emails').addClass('active');
-		// 		$('.menu-app-emails').one('click', function(e) {
-		// 			tour.next();
-		// 		});
-		// 	},
-		// 	teardown: function(tour, options) {
-		// 		$('.menu-app-emails').removeClass('active');
-		// 	}
-		// },
-
 		//Save btn
 		{
 			target: $('.save-btn'),
-			title   : '',
+			title   : 'Save Often',
 			content : 'Click here to save any changes you make by clicking here',
 			loc: "right center, left center",
-			closeButton: true,
+			highlightTarget: true,
 			setup: function(tour, options) {
+				console.log($('.save-btn'));
 				$('.save-btn').one('click', function(e) {
+					tour.next();
+				});
+			},
+			teardown: function() {
+			}
+		},
+		{
+			target  : $('.garage-toggle'),
+			title   : 'Garage',
+			content : 'Click here to go back to the garage. It will help you navigate through the site.',
+			loc: "top right, bottom right",
+			closeButton: true,
+			highlightTarget: true,
+			setup: function(tour, options) {
+				alert('ye');
+				$('.garage-toggle').one('click', function(e) {
 					tour.next();
 				});
 			},
@@ -90,34 +91,8 @@ function() {
 				$('.fixed-bg.welcome').fadeIn();
 			}
 
-		},
+		}
 
-		//Tutorial btn
-		// {
-		// 	target: $('.qm-btn#tutorial'),
-		// 	content: '<p>Click this button at any point for in-depth help</p>',
-		// 	loc: "right center, left center",
-		// 	setup: function(tour, options) {
-		// 		$('').addClass('active');
-		// 		$('.qm-btn').one('click', function(e) {
-		// 			setTimeout(tour.next, 300);
-		// 		});
-		// 	}
-		// },
-
-		// {
-		// 	content: '<p>Feel free to ask your questions here! We promise to reply really fast!</p>',
-		// 	loc: "left center, right center",
-		// 	nextButton: true,
-		// 	setup: function(tour, options) {
-		// 		util.log_to_server('finished quick tour', {}, appId);
-		// 		return { target: $('.search-bar') };
-		// 	},
-
-		// 	teardown: function() {
-		// 		$('.fixed-bg.welcome').fadeIn();
-		// 	}
-		// }
 	];
 
 
