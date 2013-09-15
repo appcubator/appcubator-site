6b6e2494af744f13a4926239080fb6732a5e0341
Karan Sikka <karanssikka@gmail.com>
2013-09-15 14:04:52 -0400

    meow

--- appcubator/static/js/application/main-app/AppRouter.js
+++ appcubator/static/js/application/main-app/AppRouter.js
@@ -215,85 +215,112 @@ define(function(require, exports, module) {
             if (v1.disableSave === true) return;
             var self = this;
             var isDeployed = false;
-            var before_deploy = new Date().getTime();
+            var before_deploy = new Date().getTime(); // global, b/c accessed in an ajax handler
             v1.disableSave = true;
+
+            console.log("in deploy");
+
+            var successHandler = function(data, callback){
+                console.log("in success handler");
+                v1.whenDeployed(function() {
+                    callback.call();
+                    console.log("in when deployed");
+                    new DeployView(data);
+                    util.log_to_server('deployed app', {
+                        status: 'success',
+                        deploy_time: data.deploy_time + " seconds"
+                    }, appId);
+                    self.trigger('deployed');
+                });
+                console.log("called callback");
+                return data;
+            };
+
+            var jqxhrToJson = function(jqxhr){
+                console.log("in jqxhr i got this data");
+                console.log(jqxhr);
+                var data = {};
+                try {
+                    data = JSON.parse(jqxhr.responseText);
+                } catch (e) {
+                    data.errors = ["JSON response from server failed to parse"];
+                }
+                console.log("in jqxhr i return this data");
+                console.log(data);
+                return data;
+            };
+
+            var mergeConflictHandler = function(data){
+                var text = "<h1>Merge Conflict</h1>";
+                text += "\n<p>We tried to generate the code but we couldn't resolve a conflict between our code and your code.</p>";
+                text += "\n<p>To fix this, please resolve the conflict and push a commit with your fix in <span class=\"branch\">master</span>.</p>";
+                text += "\n<p>We stored the conflict details in <span class=\"branch\">" + data.branch + "</span>.</p>";
+                text += "\n<div>";
+                text += "\n  <h2>Affected files</h2>";
+                text += "\n  <ol>";
+                for (var i = 0; i < data.files.length; i++) {
+                    text += "\n    <li class=\"file\">" + data.files[i] + "</li>";
+                }
+                text += "\n  </ol>";
+                text += "\n<div>";
+
+                var content = {
+                    text: text
+                };
+                new SimpleModalView(content);
+                util.log_to_server('deployed app', {
+                    status: 'merge conflict',
+                    deploy_time: data.deploy_time + " seconds",
+                    message: data
+                }, appId);
+                return data;
+            };
+
+            // this is copy pasted from the save code. i dont know how to modularize these functions properly. ~ks
+            var softErrorHandler = function(data) {
+                v1State.set('version_id', data.version_id);
+                v1.disableSave = true;
+                new SoftErrorView({
+                    text: data.message,
+                    path: data.path
+                }, function() {
+                    v1.disableSave = false;
+                });
+                return data;
+            };
+            var hardErrorHandler = function(data){
+                var content = {};
+                if (DEBUG) content.text = data.errors;
+                else content.text = "There has been a problem. Please refresh your page. We're really sorry for the inconvenience and will be fixing it very soon.";
+                new ErrorDialogueView(content);
+                util.log_to_server('deployed app', {
+                    status: 'FAILURE',
+                    deploy_time: data.deploy_time + " seconds",
+                    message: data.errors
+                }, appId);
+                return data;
+            };
+
+            // compose this w the other callbacks
+            var completeCallback = function(data) {
+                console.log("in complete callback i got this data");
+                console.log(data);
+                v1.disableSave = false;
+                isDeployed = true;
+                data.deploy_time = (new Date().getTime() - before_deploy) / 1000;
+                console.log("in complete callback i return this data");
+                console.log(data);
+                return data
+            };
 
             $.ajax({
                 type: "POST",
                 url: '/app/' + appId + '/deploy/',
-                success: function(data) {
-                    v1.disableSave = false;
-                    isDeployed = true;
-                    var deploy_time = (new Date().getTime() - before_deploy) / 1000;
-                    if (callback) v1.whenDeployed(callback);
-                    // open a modal based on deploy response
-                    if (data.errors) {
-                        var content = {
-                            text: "There has been a problem. Please refresh your page. We're really sorry for the inconvenience and will be fixing it very soon."
-                        };
-                        if (DEBUG) {
-                            content = {
-                                text: data.errors
-                            };
-                        }
-                        new ErrorDialogueView(content);
-                        util.log_to_server('deployed app', {
-                            status: 'FAILURE',
-                            deploy_time: deploy_time + " seconds",
-                            message: data.errors
-                        }, appId);
-                    } else if (data.files && data.branch) {
-                        var text = "<h1>Merge Conflict</h1>";
-                        text += "\n<p>We tried to generate the code but we couldn't resolve a conflict between our code and your code.</p>";
-                        text += "\n<p>To fix this, please resolve the conflict and push a commit with your fix in <span class=\"branch\">master</span>.</p>";
-                        text += "\n<p>We stored the conflict details in <span class=\"branch\">" + data.branch + "</span>.</p>";
-                        text += "\n<div>";
-                        text += "\n  <h2>Affected files</h2>";
-                        text += "\n  <ol>";
-                        for (var i = 0; i < data.files.length; i++) {
-                            text += "\n    <li class=\"file\">" + data.files[i] + "</li>";
-                        }
-                        text += "\n  </ol>";
-                        text += "\n<div>";
-
-                        var content = {
-                            text: text
-                        };
-                        new SimpleModalView(content);
-                        util.log_to_server('deployed app', {
-                            status: 'merge conflict',
-                            deploy_time: deploy_time + " seconds",
-                            message: data
-                        }, appId);
-                    } else {
-                        v1.whenDeployed(function() {
-                            new DeployView(data);
-                            util.log_to_server('deployed app', {
-                                status: 'success',
-                                deploy_time: deploy_time + " seconds"
-                            }, appId);
-                            self.trigger('deployed');
-                        });
-                    }
-                },
-                error: function(data) {
-                    v1.disableSave = false;
-                    isDeployed = true;
-                    var deploy_time = (new Date().getTime() - before_deploy) / 1000;
-                    var content = {
-                        text: "There has been a problem. Please refresh your page. We're really sorry for the inconvenience and will be fixing it very soon."
-                    };
-                    if (DEBUG) {
-                        content = {
-                            text: data.responseText
-                        };
-                    }
-                    new ErrorDialogueView(content);
-                    util.log_to_server('deployed app', {
-                        status: 'FAILURE',
-                        deploy_time: deploy_time + " seconds",
-                        message: data.responseText
-                    }, appId);
+                statusCode: {
+                    200: _.compose(completeCallback, function(data){ return successHandler(data, callback); }),
+                    400: _.compose(jqxhrToJson, completeCallback, softErrorHandler, callback),
+                    409: _.compose(jqxhrToJson, completeCallback, mergeConflictHandler, callback),
+                    500: _.compose(jqxhrToJson, completeCallback, hardErrorHandler, callback),
                 },
                 dataType: "JSON"
             });
