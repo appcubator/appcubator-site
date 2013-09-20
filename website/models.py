c4dd505f3f68b0ca58f8d5292aa95646773d9e2c
v1 Factory <founders@v1factory.com>
2013-09-20 08:25:42 +0000

    db fixes

--- website/models.py
+++ website/models.py
@@ -6,7 +6,7 @@ from django.contrib.contenttypes.models import ContentType
 
 
 class Document(models.Model):
-    title = models.TextField(max_length=255, null=True, blank=True)
+    title = models.CharField(max_length=255, null=True, blank=True)
     added_date = models.DateTimeField(auto_now_add=True)
     
     class Meta:
@@ -15,10 +15,10 @@ class Document(models.Model):
 class Love(models.Model):
     "A user or session owned indication of love for some object. Either user or session should be present to indicate the owner."
     content_type = models.ForeignKey(ContentType)
-    object_pk = models.TextField(max_length=255)
+    object_pk = models.CharField(max_length=255)
     content_object = generic.GenericForeignKey(ct_field="content_type", fk_field="object_pk")
     user = models.ForeignKey(User, null=True, blank=True)
-    session_key = models.TextField(max_length=255, null=True, blank=True)
+    session_key = models.CharField(max_length=255, null=True, blank=True)
     added_date = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
