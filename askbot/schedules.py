10d853c259d432e5d33cf80565524206d7d41b7f
v1 Factory <founders@v1factory.com>
2013-09-22 02:23:32 +0000

    fix pytz issue

--- askbot/schedules.py
+++ askbot/schedules.py
@@ -1,6 +1,7 @@
 """tests on whether certain scheduled tasks need
 to be performed at the moment"""
 from datetime import datetime
+import pytz
 
 def should_update_avatar_data(request):
     """True if it is time to update user's avatar data
@@ -8,12 +9,12 @@ def should_update_avatar_data(request):
     """
     user = request.user
     if user.is_authenticated():
-        if (datetime.today() - user.last_login).days <= 1:
+        if (pytz.UTC.localize(datetime.today()) - user.last_login).days <= 1:
             #avatar is updated on login anyway
             return False
         updated_at = request.session.get('avatar_data_updated_at', None)
         if updated_at is None:
             return True
         else:
-            return (datetime.now() - updated_at).days > 0
+            return (pytz.UTC.localize(datetime.now()) - updated_at).days > 0
     return False
