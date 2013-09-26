4dc25831f8015d390b7d3e63c54627812358c210
Ilter Canberk <ilteriscanberk@gmail.com>
2013-09-26 18:15:29 -0400

    custom widget improvements

--- appcubator/static/js/application/main-app/editor/CustomWidgetEditorModal.js
+++ appcubator/static/js/application/main-app/editor/CustomWidgetEditorModal.js
@@ -1,56 +1,75 @@
 define([
-  'mixins/BackboneModal',
-  'ace'
-],
-function() {
+        'mixins/BackboneModal',
+        'ace'
+    ],
+    function() {
 
-  var CustomWidgetEditorModal = Backbone.ModalView.extend({
-    className : 'custom-widget-editor',
-    width  : 540,
-    height: 540,
-    title: "Custom Widget Editor",
-    doneButton: true,
+        var CustomWidgetEditorModal = Backbone.ModalView.extend({
+            className: 'custom-widget-editor',
+            width: 540,
+            height: 540,
+            padding: 0,
+            title: "Custom Widget Editor",
+            doneButton: true,
 
-    events : {
-    },
+            events: {
+                'click .sub-title' : 'toggle',
+            },
 
-    initialize: function(widgetModel){
-      _.bindAll(this);
-      this.model = widgetModel;
-      this.render();
-    },
+            initialize: function(widgetModel) {
+                _.bindAll(this);
+                this.model = widgetModel;
+                this.render();
+            },
 
-    render: function() {
-      var self = this;
-      var htmlStr = this.model.get('data').get('htmlC')||'';
-      var cssStr  = this.model.get('data').get('cssC')||'';
-      var jsStr   = this.model.get('data').get('jsC')||'';
+            render: function() {
+                var self = this;
+                var htmlStr = this.model.get('data').get('htmlC') || '';
+                var cssStr = this.model.get('data').get('cssC') || '';
+                var jsStr = this.model.get('data').get('jsC') || '';
 
-      this.el.innerHTML = 'HTML<br><div id="edit-html-inp" style="background-color:#eee; height: 100px; width:480px; position:relative;"></div><br>JS<br><div id="edit-js-inp" style="position:relative; background-color:#eee; height: 100px; width:480px;"></div><br>CSS<br><div id="edit-css-inp" style="background-color:#eee; height: 100px; width:480px;"></div>';
-      
-      this.CSSeditor = ace.edit("edit-css-inp");
-      this.CSSeditor.getSession().setMode("ace/mode/css");
-      this.CSSeditor.setValue(cssStr, -1);
+                var content = [
+                    '<div class="sect"><div class="sub-title">» HTML</div><div id="edit-html-inp" style="background-color:#eee; height: 300px; width:540px; position:relative;"></div></div>',
+                    '<div class="sect"><div class="sub-title">» JS</div><div id="edit-js-inp" style="position:relative; background-color:#eee; height: 300px; width:540px;"></div></div>',
+                    '<div class="sect"><div class="sub-title">» CSS</div><div id="edit-css-inp" style="position:relative; background-color:#eee; height: 300px; width:540px;"></div></div>',
+                    '<a style="width:100%; display:block; text-align: center; padding: 8px; color: #666;" href="/resources/tutorial/custom-widget/" rel="external" target="_blank">Guide on using the Custom Widget</a>'
+                ].join('\n');
 
-      this.HTMLeditor = ace.edit("edit-html-inp");
-      this.HTMLeditor.getSession().setMode("ace/mode/html");
-      this.HTMLeditor.setValue(htmlStr, -1);
+                this.el.innerHTML = content;
 
-      this.JSeditor = ace.edit("edit-js-inp");
-      this.JSeditor.getSession().setMode("ace/mode/javascript");
-      this.JSeditor.setValue(jsStr, -1);
+                this.CSSeditor = ace.edit("edit-css-inp");
+                this.CSSeditor.getSession().setMode("ace/mode/css");
+                this.CSSeditor.setValue(cssStr, -1);
 
-      return this;
-    },
+                this.HTMLeditor = ace.edit("edit-html-inp");
+                this.HTMLeditor.getSession().setMode("ace/mode/html");
+                this.HTMLeditor.setValue(htmlStr, -1);
 
-    onClose: function() {
-      this.model.get('data').set('cssC', this.CSSeditor.getValue());
-      this.model.get('data').set('jsC', this.JSeditor.getValue());
-      this.model.get('data').set('htmlC', this.HTMLeditor.getValue());
-      this.model.trigger('custom_edited');
-    }
+                this.JSeditor = ace.edit("edit-js-inp");
+                this.JSeditor.getSession().setMode("ace/mode/javascript");
+                this.JSeditor.setValue(jsStr, -1);
 
-  });
+                return this;
+            },
 
-  return CustomWidgetEditorModal;
-});
+            toggle: function(e) {
+                if($(e.currentTarget.parentNode).hasClass('expanded')) return this.shrink(e);
+                this.$el.find('.expanded').removeClass('expanded');
+                $(e.currentTarget.parentNode).addClass('expanded');
+            },
+
+            shrink: function(e) {
+                $(e.currentTarget.parentNode).removeClass('expanded');
+            },
+
+            onClose: function() {
+                this.model.get('data').set('cssC', this.CSSeditor.getValue());
+                this.model.get('data').set('jsC', this.JSeditor.getValue());
+                this.model.get('data').set('htmlC', this.HTMLeditor.getValue());
+                this.model.trigger('custom_edited');
+            }
+
+        });
+
+        return CustomWidgetEditorModal;
+    });
