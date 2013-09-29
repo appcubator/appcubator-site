be036cdc4558a8ddae3d8f42f26e28594e0fbec4
vagrant <karanssikka@gmail.com>
2013-09-29 21:03:58 +0000

    fixed migrations + initial data

--- appcubator/plugins/migrations/0002_initial_provider_data.py
+++ appcubator/plugins/migrations/0002_initial_provider_data.py
@@ -9,7 +9,7 @@ class Migration(DataMigration):
     def forwards(self, orm):
         "Write your forwards methods here."
         from django.core.management import call_command
-        call_command('loaddata', 'providers.json')
+        call_command('loaddata', 'providers')
 
     def backwards(self, orm):
         "Write your backwards methods here."
