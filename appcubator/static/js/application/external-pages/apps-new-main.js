a8bb2e87da9a996de72f15874d2655bbf89ab5de
Ilter Canberk <ilteriscanberk@gmail.com>
2013-09-30 16:51:58 -0400

    Works well on other pages

--- appcubator/static/js/application/external-pages/apps-new-main.js
+++ appcubator/static/js/application/external-pages/apps-new-main.js
@@ -1,75 +1,84 @@
 require.config({
-  paths: {
-    "jquery" : "../../libs/jquery/jquery",
-    "bootstrap" : "../../libs/bootstrap/bootstrap",
-    "app"       : "../main-app",
-    "util"      : "../../libs/util/util",
-    "mixins"          : "../../mixins",
-    "backbone"        : "../../libs/backbone-amd/backbone",
-    "underscore"      : "../../libs/underscore-amd/underscore",
-    "jquery-ui"       : "../../libs/jquery-ui/jquery-ui"
-  },
+    paths: {
+        "jquery": "../../libs/jquery/jquery",
+        "bootstrap": "../../libs/bootstrap/bootstrap",
+        "app": "../main-app",
+        "util": "../../libs/util/util",
+        "mixins": "../../mixins",
+        "backbone": "../../libs/backbone-amd/backbone",
+        "underscore": "../../libs/underscore-amd/underscore",
+        "jquery-ui": "../../libs/jquery-ui/jquery-ui"
+    },
 
-  shim: {
-    "bootstrap" : {
-      deps: ["jquery"]
-    },
-    "backbone": {
-      exports: "Backbone",
-      deps: ["underscore", "jquery"]
-    },
-    "underscore": {
-      exports: "_"
+    shim: {
+        "bootstrap": {
+            deps: ["jquery"]
+        },
+        "backbone": {
+            exports: "Backbone",
+            deps: ["underscore", "jquery"]
+        },
+        "underscore": {
+            exports: "_"
+        }
     }
-  }
 
 });
 
 require([
-  'jquery',
-  '../main-app/GarageView',
-  'util'
-],
-function($, GarageView) {
+        'jquery',
+        '../main-app/GarageView',
+        '../main-app/WorldView',
+        'util'
+    ],
+    function($, GarageView, WorldView) {
 
-  var currentTemplate = null;
+        var currentTemplate = null;
 
-  $(function () {
-      $('form').on('submit', function(e) {
-        $(e.target).on('submit', function(e) {
-          e.preventDefault();
+        $(function() {
+            $('form').on('submit', function(e) {
+                $(e.target).on('submit', function(e) {
+                    e.preventDefault();
+                });
+            });
         });
-      });
-  });
 
-  $('#skip-racoon').on('click', function() {
-    var url = "/app/new/";
-    if(currentTemplate) {
-      url = url + "template/" +currentTemplate + "/";
-      util.log_to_server("app template", currentTemplate, 0);
-    }
-    $('form').attr("action", url);
-    $('form').submit();
-  });
+        $('#skip-racoon').on('click', function() {
+            var url = "/app/new/";
+            if (currentTemplate) {
+                url = url + "template/" + currentTemplate + "/";
+                util.log_to_server("app template", currentTemplate, 0);
+            }
+            $('form').attr("action", url);
+            $('form').submit();
+        });
 
-  $('.app-template').click(function(e) {
-    if($(e.currentTarget).hasClass('selected')) {
-      currentTemplate = null;
-      $('.selected').removeClass('selected');
-    }
-    else {
-      $('.selected').removeClass('selected');
-      $(e.currentTarget).addClass('selected');
-      currentTemplate = e.currentTarget.id;
-    }
-  });
+        $('.app-template').click(function(e) {
+            if ($(e.currentTarget).hasClass('selected')) {
+                currentTemplate = null;
+                $('.selected').removeClass('selected');
+            } else {
+                $('.selected').removeClass('selected');
+                $(e.currentTarget).addClass('selected');
+                currentTemplate = e.currentTarget.id;
+            }
+        });
 
-  $('#guide-btn').hover(function() {
-    $('#mascot').addClass('happy');
-  }, function() {
-    $('#mascot').removeClass('happy');
-  });
+        $('#guide-btn').hover(function() {
+            $('#mascot').addClass('happy');
+        }, function() {
+            $('#mascot').removeClass('happy');
+        });
 
-  var garage = new GarageView();
-  $('.garage-toggle').on('click', garage.show);
-});
+        this.worldView = new WorldView();
+
+        // if (appId !== 0) {
+            this.garageView = new GarageView();
+            $('.garage-toggle').on('click', this.garageView.toggle);
+            $('.garage-toggle').on('click', this.worldView.hide);
+            $('.world-toggle').on('click', this.garageView.hide);
+        // }
+
+        $('.world-toggle').on('click', this.worldView.toggle);
+
+    });
