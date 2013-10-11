38c5dfb8ce3d173c92fe1f2a9be0dde05bcdb4a2
Karan Sikka <karanssikka@gmail.com>
2013-10-11 18:26:21 -0400

    cleaned outer directory

---
+++ scripts/upgrade.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+clear
+echo "Entering the matrix..."
+echo "......................"
+echo "......................"
+#cd ../appcubator-codegen
+#git pull
+#cd ../appcubator-site
+pip install ../appcubator-codegen --upgrade
