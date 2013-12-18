8c021a8e73f3c4f174f24b9be50d752f7ab80bf3
Ilter Canberk <ilteriscanberk@gmail.com>
2013-12-18 17:13:09 -0500

    theme picker design

--- appcubator/static/js/application/main-app/ThemesGalleryView.js
+++ appcubator/static/js/application/main-app/ThemesGalleryView.js
@@ -1,60 +1,72 @@
-define([
-  'app/ThemeDisplayView',
-  'backbone',
-  'util'
-],
-function(ThemeDisplayView) {
+define(function(require, exports, module) {
 
-  var ThemesGalleryView = Backbone.View.extend({
-    css: 'gallery',
-    events: {
-      'click .theme' : 'showThemeModal'
-    },
+    'use strict';
 
-    className: 'gallery-view',
+    require('backbone');
+    require('util');
+    var ThemeDisplayView = require('app/ThemeDisplayView');
 
-    initialize: function() {
-      this.title = "Themes";
-    },
 
-    render: function() {
-      this.listView = document.createElement('ul');
-      this.listView.className = 'theme-gallery';
-
-      var template = [
-        '<li class="theme" class="theme-item" id="theme-<%= id %>">',
-          '<h2><%= name %></h2>',
-          '<p class="designed-by">Designed by <%= designer %></p>',
-          '<div class="img"><img src="<%= image %>"></div>',
-          '<div class="details">Click to See Details</div>',
-        '</li>'
-      ].join('\n');
-
-      _(themes).each(function(theme, index) {
-        if(!theme.name) {
-          theme.name = "Theme " + index;
-        }
-        this.listView.innerHTML += _.template(template, theme);
-      }, this);
-
-      $(this.el).append(this.listView);
-
-      return this;
-    },
-
-    showThemeModal: function(e) {
-      var themeId = e.target.parentNode.id.replace('theme-','');
-      $.ajax({
-        type: "POST",
-        url: '/theme/'+themeId+'/info/',
-        success: function(data) {
-          new ThemeDisplayView(data, themeId);
+    var ThemesGalleryView = Backbone.View.extend({
+        css: 'gallery',
+        events: {
+            'click      .theme': 'showThemeModal',
+            'mouseover  .theme': 'previewTheme',
+            'mouseleave .theme': 'revertTheme'
         },
-        dataType: "JSON"
-      });
-    }
 
-  });
+        className: 'gallery-view',
 
-  return ThemesGalleryView;
+        initialize: function() {
+            this.title = "Themes";
+        },
+
+        render: function() {
+            this.listView = document.createElement('ul');
+            this.listView.className = 'theme-gallery';
+
+            var template = [
+                '<li class="theme" class="theme-item" id="theme-<%= id %>">',
+                '<h2><%= name %></h2>',
+                '<p class="designed-by">Designed by <%= designer %></p>',
+                '<div class="img"><img src="<%= image %>"></div>',
+                '<div class="details">Click to See Details</div>',
+                '</li>'
+            ].join('\n');
+
+            _(themes).each(function(theme, index) {
+                if (!theme.name) {
+                    theme.name = "Theme " + index;
+                }
+                this.listView.innerHTML += _.template(template, theme);
+            }, this);
+
+            $(this.el).append(this.listView);
+
+            return this;
+        },
+
+        showThemeModal: function(e) {
+            var themeId = e.target.parentNode.id.replace('theme-', '');
+            $.ajax({
+                type: "POST",
+                url: '/theme/' + themeId + '/info/',
+                success: function(data) {
+                    new ThemeDisplayView(data, themeId);
+                },
+                dataType: "JSON"
+            });
+        },
+
+        previewTheme: function() {
+
+        },
+
+        revertTheme: function() {
+
+        }
+
+    });
+
+    return ThemesGalleryView;
 });
