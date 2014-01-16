1e3fa7895e3414003f51d62e6db11230826b441d
Karan Sikka <karanssikka@gmail.com>
2014-01-16 16:59:10 -0500

    mmm

--- appcubator/deploy/deis_deploy.py
+++ appcubator/deploy/deis_deploy.py
@@ -8,9 +8,6 @@ logger = logging.getLogger(__name__)
 
 from deis import DeisClient
 
-"""
-TODO replace with a deis implementation
-"""
 
 class DeploymentError(Exception):
     """Should be raised whenever the deployment server does not return 200"""
@@ -21,16 +18,8 @@ class NotDeployedError(Exception):
     pass
 
 def update_deployment_info(deployment_id, hostname):
-    payload = { 'hostname': hostname }
-    deployment_url = 'http://%s/deployment/%d/info/' % (settings.DEPLOYMENT_HOSTNAME, deployment_id)
-    r = requests.post(deployment_url, data=payload, headers={'X-Requested-With': 'XMLHttpRequest'})
-    if r.status_code == 200:
-        return r
-    elif r.status_code == 404:
-        raise NotDeployedError()
-    else:
-        logger.error("Update deployment info failed: %r" % r.__dict__)
-        raise DeploymentError()
+    """Update the deployment with the new hostname"""
+    pass
 
 def get_deployment_status(deployment_id):
     """
@@ -38,34 +27,13 @@ def get_deployment_status(deployment_id):
      0 = No task running
      1 = Running
      2 = Task done, plz collect result.
+    #TODO async
     """
-    deployment_url = 'http://%s/deployment/%d/task/status/' % (settings.DEPLOYMENT_HOSTNAME, deployment_id)
-    r = requests.get(deployment_url, headers={'X-Requested-With': 'XMLHttpRequest'})
-    if r.status_code != 200:
-        logger.error("Get deployment status failed: %r" % r.__dict__)
-        if r.status_code == 404:
-            raise NotDeployedError()
-        else:
-            raise DeploymentError()
+    return random.randint(0,1) * 2
 
-    # get data out
-    d = r.json()
-    status = d['status']
-    message = d['message'] # not doing anything w this yet but know it exists.
-    assert status in (0, 1, 2)
-    if status == 2:
-        # clear the result from the server so next time it will be 0
-        deployment_url = 'http://%s/deployment/%d/task/result/' % (settings.DEPLOYMENT_HOSTNAME, deployment_id)
-        r2 = requests.get(deployment_url, headers={'X-Requested-With': 'XMLHttpRequest'})
-
-    return status
-
-
-#def _write_tar_from_app_dir(self, appdir):
-def write_tar_from_app_dir(appdir):
-    """
-    Given the directory of the app, tar it up and return the path to the tar.
-    """
+# tar it up
+def _write_tar_from_app_dir(appdir):
+    """Given the directory of the app, tar it up and return the path to the tar."""
     contents = os.listdir(appdir)
     # tar it up
     t = tarfile.open(os.path.join(appdir, 'payload.tar'), 'w')
@@ -74,72 +42,78 @@ def write_tar_from_app_dir(appdir):
     t.close()
     return os.path.join(appdir, 'payload.tar')
 
-def transport_app(appdir, deploy_id, deploy_data, retry_on_404=True):
+def provision(appdir, deploy_data):
+    """
+    Builds a development sandbox on a Deis server.
+    Returns deployment_id
+    """
+    tar = _write_tar_from_app_dir(appdir)
+    with open(tar, rb) as f:
+        r = requests.post(DEIS_URL+'/sandbox/', files={'code':f})
+
+    if r.status_code == 200:
+        return r.json()['id']
+    else:
+        raise DeploymentError(str(r.status_code) + ': ' + r.text)
+
+def update_code(appdir, deploy_id, deploy_data):
     """
     1. path to directory where app is stored
     2. deployment id (or none if it's not yet deployed)
     3. dictionary of deploy data
             "hostname": app.hostname(),
-            "app_json": app.state_json,
+            "url": 'http://' + app.hostname() + '/', (reachable url)
             "deploy_secret": "v1factory rocks!",
-
-    returns false, deployment_id
-    or
-    returns true, { files: [] , branch: "" }
-
-    might raise DeploymentError or AssertionError.
     """
-    # tar it up
-    tar_path = write_tar_from_app_dir(appdir)
-    f = open(tar_path, "r")
+    tar_path = _write_tar_from_app_dir(appdir)
+    f = open(tar_path, "rb")
     try:
-        # catapult the tar over to the deployment server
-        files = {'file':f}
-        post_data = deploy_data
-        if deploy_id is None:
-            try:
-                r = requests.post("http://%s/deployment/" % settings.DEPLOYMENT_HOSTNAME, data=post_data, files=files, headers={'X-Requested-With': 'XMLHttpRequest'})
-            except Exception:
-                raise
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        host = deploy_data['hostname']
+        port = deploy_data.get('_port', 80)
+        s.connect((host, port))
 
-        else:
-            r = requests.post("http://%s/deployment/%d/" % (settings.DEPLOYMENT_HOSTNAME, deploy_id), data=post_data, files=files, headers={'X-Requested-With': 'XMLHttpRequest'})
+        tarbin = base64.b64encode(f.read())
+        s.sendall("POST /__update_code__ HTTP/1.1\n\n" + str(len(tarbin))+"\n\n")
+        s.sendall(tarbin)
+        s.recv(1)
+
+        s.close()
 
     finally:
         f.close()
         os.remove(os.path.join(appdir, 'payload.tar'))
 
-    if r.status_code == 200:
-        result = {}
-        response_content = r.json()
-        logger.debug("Deployment response content: %r" % response_content)
-        if 'deployment_id' not in response_content:
-            raise DeploymentError("Deployment server had errors: %r" % response_content['errors'])
 
-        # weirdo sanity check to make sure migration worked ok.
-        try:
-            syncdb_data = [ u for u in response_content['script_results'] if 'syncdb.py' in u['script'] ][0]
-            if u'value to use for existing rows' in syncdb_data['stderr']:
-                assert False, "Migration needs help!!!"
-        except Exception:
-            pass # this is the fast_deploy case
+import unittest
+from appcubator import codegen
+from appcubator.default_data import get_default_app_state
+import shutil
+import json
+APPSTATE = json.loads(get_default_app_state())
 
-        return (False, response_content['deployment_id'])
 
-    elif r.status_code == 404 or (r.status_code == 502 and requests.get("http://%s/lskdjflskjf/" % settings.DEPLOYMENT_HOSTNAME).status_code == 404):
-        assert retry_on_404
-        logger.warn("The deployment was not found, so I'm setting deployment id to None")
-        return transport_app(appdir, None, deploy_data, retry_on_404=False)
+## Testing requires an example app. We use the default state + codegen
+## Note: CODEGEN SERVER MUST BE LIVE TO TEST.
+class TestProvision(unittest.TestCase):
 
-    # merge conflict with custom code
-    elif r.status_code == 409:
-        result = {}
-        response_content = r.json()
-        logger.debug("Deployment response content: %r" % response_content)
-        result['files'] = response_content['files']
-        result['branch'] = response_content['branch']
+    def setUp(self):
+        self.appdir = codegen.write_to_tmpdir(codegen.compileApp(APPSTATE))
 
-        return (True, result)
+    def test_provision(self):
+        self.id = random.randint(100, 999)
+        dd = { 'hostname': 'testing%d.app.com' % self.id,
+               'url': 'http://testing%d.app.com/' % self.id }
+        d_id = provision(self.appdir, dd)
+        p, port = fake_database[d_id]
+        import time; time.sleep(2)
+        print "Devmon pid=" + str(p.pid)
+        r = requests.get('http://127.0.0.1:%d' % port)
+        self.assertEqual(r.status_code, 200)
 
-    else:
-        raise DeploymentError("Deployment server error: %r" % r.text)
+    def tearDown(self):
+        shutil.rmtree(self.appdir)
+
+if __name__ == "__main__":
+    unittest.main()
+
