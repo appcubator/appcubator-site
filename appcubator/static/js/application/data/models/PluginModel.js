db841c775dae4dcbbfea29bfc4bf6a861e920178
Karan Sikka <karanssikka@gmail.com>
2014-02-18 22:51:21 -0500

    deadcode

--- appcubator/static/js/application/data/models/PluginModel.js
+++ appcubator/static/js/application/data/models/PluginModel.js
@@ -16,18 +16,6 @@ define([
 
         },
 
-        fetchPlugin: function() {
-            var name = this.get("name");
-            var pluginAddress = "http://localhost:3001/package/" + name;
-            $.ajax({
-                url: pluginAddress,
-                success: function(generators) {
-                    console.log(generators);
-                    //this.set('modules',  generators)
-                }.bind(this)
-            });
-        },
-
         getGeneratorsWithModule: function(moduleName) {
             if(!this.has(moduleName)) return [];
 
