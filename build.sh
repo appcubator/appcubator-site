55fce1750fee32e8767b1e391448cedacec27c68
Karan Sikka <karanssikka@gmail.com>
2014-02-24 01:58:23 -0500

    fixing

--- build.sh
+++ build.sh
@@ -19,10 +19,15 @@ lessc --verbose --rootpath=/static/css/ -x --yui-compress --ru --line-numbers=me
 echo "[BUILD] Compiled external-website.less to external-website.css"
 
 # fix /static/
-sed -i css 's/@import "\/static\//@import "https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/app/app.css
-sed -i css 's/@import "\/static\//@import "https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/internal.css
-sed -i css 's/@import "\/static\//@import "https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/documentation.css
-sed -i css 's/@import "\/static\//@import "https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/external-website.css
+#sed -i css 's/@import "\/static\//@import "https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/app/app.css
+#sed -i css 's/@import "\/static\//@import "https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/internal.css
+#sed -i css 's/@import "\/static\//@import "https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/documentation.css
+#sed -i css 's/@import "\/static\//@import "https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/external-website.css
+#
+#sed -i css 's/\/static\//https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/app/app.css
+#sed -i css 's/\/static\//https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/internal.css
+#sed -i css 's/\/static\//https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/documentation.css
+#sed -i css 's/\/static\//https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/external-website.css
 
 # compile javascript
 
