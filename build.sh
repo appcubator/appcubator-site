56721a372352993b5fa3ac2104224623336d12f3
Karan Sikka <karanssikka@gmail.com>
2014-02-24 02:16:47 -0500

    holy cow it works

--- build.sh
+++ build.sh
@@ -9,25 +9,25 @@ pushd $DIR/appcubator
 # compile less/
 cp ./static/js/application/main-app/main.js ./static/js/application/main-app/main.js.backup
 echo "[BUILD] Copied main.js to main.js.backup"
-lessc --verbose --rootpath=/static/css/app/ -x --yui-compress --ru --line-numbers=mediaquery ./static/css/app/app.less ./static/css/app/app.css
+lessc --verbose --rootpath=/static/css/app/ -x --yui-compress --ru --line-numbers=mediaquery ./static/css/app/app.less ./dist_static/css/app/app.css
 echo "[BUILD] Compiled app/style.less to app.css"
-lessc --verbose --rootpath=/static/css/app/ -x --yui-compress --ru --line-numbers=mediaquery ./static/css/internal.less ./static/css/internal.css
+lessc --verbose --rootpath=/static/css/app/ -x --yui-compress --ru --line-numbers=mediaquery ./static/css/internal.less ./dist_static/css/internal.css
 echo "[BUILD] Compiled app/style.less to internal.css"
-lessc --verbose --rootpath=/static/css/ -x --yui-compress --ru --line-numbers=mediaquery ./static/css/documentation.less ./static/css/documentation.css
+lessc --verbose --rootpath=/static/css/ -x --yui-compress --ru --line-numbers=mediaquery ./static/css/documentation.less ./dist_static/css/documentation.css
 echo "[BUILD] Compiled documentation.less to documentation.css"
-lessc --verbose --rootpath=/static/css/ -x --yui-compress --ru --line-numbers=mediaquery ./static/css/external-website.less ./static/css/external-website.css
+lessc --verbose --rootpath=/static/css/ -x --yui-compress --ru --line-numbers=mediaquery ./static/css/external-website.less ./dist_static/css/external-website.css
 echo "[BUILD] Compiled external-website.less to external-website.css"
 
 # fix /static/
-#sed -i css 's/@import "\/static\//@import "https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/app/app.css
-#sed -i css 's/@import "\/static\//@import "https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/internal.css
-#sed -i css 's/@import "\/static\//@import "https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/documentation.css
-#sed -i css 's/@import "\/static\//@import "https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/external-website.css
-#
-#sed -i css 's/\/static\//https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/app/app.css
-#sed -i css 's/\/static\//https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/internal.css
-#sed -i css 's/\/static\//https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/documentation.css
-#sed -i css 's/\/static\//https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/external-website.css
+#sed -i 's/@import "\/static\//@import "https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/app/app.css
+#sed -i 's/@import "\/static\//@import "https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/internal.css
+#sed -i 's/@import "\/static\//@import "https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/documentation.css
+#sed -i 's/@import "\/static\//@import "https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./static/css/external-website.css
+
+sed -i '' 's/\/static\//https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./dist_static/css/app/app.css
+sed -i '' 's/\/static\//https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./dist_static/css/internal.css
+sed -i '' 's/\/static\//https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./dist_static/css/documentation.css
+sed -i '' 's/\/static\//https:\/\/s3.amazonaws.com\/appcubator-hosting\//g' ./dist_static/css/external-website.css
 
 # compile javascript
 
