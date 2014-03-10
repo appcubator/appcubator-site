9364b24fcf5799127845ff6678a195b6581c1237
Karan Sikka <karanssikka@gmail.com>
2014-03-10 01:29:28 -0700

    dead code

--- appcubator/static/js/application/main-app/template_editor/EditorGalleryView.js
+++ appcubator/static/js/application/main-app/template_editor/EditorGalleryView.js
@@ -16,69 +16,7 @@ define(function(require, exports, module) {
     /* uielement.displayProps.iconType may be one of these values, which happen to be class names for sprites . */
     var validIconClasses = ['button', 'image', 'header', 'text',
                             'link', 'line', 'box', 'imageslider',
-                            'fbshare', 'embedvideo'];
-
-    var defaultElements = [
-      {
-        text: "Button",
-        className : "button",
-        el : "<div class='btn'>Default Button</div>"
-      },
-
-      {
-        text: "Image",
-        className : "image",
-        el : "<img class='span16' src='/static/img/placeholder.png'>"
-      },
-
-      {
-        text: "Header",
-        className : "header",
-        el : "<h1>Default Header</h1>"
-      },
-
-      {
-        text: "Text",
-        className : "text",
-        el : "<did>Default text!</div>"
-      },
-
-      {
-        text: "Link",
-        className : "link",
-        el : "<a href='#'>Default Link</div>"
-      },
-
-      {
-        text: "Line",
-        className : "line",
-        el : "<hr class='span20'>"
-      },
-
-      {
-        text: "Box",
-        className : "box",
-        el : "<div style='background-color:#ccc;height:120px' class='span20'></div>"
-      },
-
-      {
-        text: "Image Slider",
-        className : "imageslider",
-        el : "<img class='span24' src='/static/img/placeholder.png'>"
-      },
-
-      {
-        text: "FB Share",
-        className : "fbshare",
-        el : "<img src='/static/img/fb-share-sample.png'>"
-      },
-
-      {
-        text: "Embed Video",
-        className : "embedvideo",
-        el : "<img src='/static/img/youtube-static.png'>"
-      }
-    ];
+                            'fbshare', 'embedvideo', 'custom-widget'];
 
     var EditorGalleryView = Backbone.View.extend({
 
