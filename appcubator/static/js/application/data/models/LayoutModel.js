cd63a2509a0850c2a01dc57dc417dfe1f9c25ce7
Ilter Canberk <iltercanberk@gmail.com>
2014-03-20 23:03:20 -0400

    Cleanup

--- appcubator/static/js/application/data/models/LayoutModel.js
+++ appcubator/static/js/application/data/models/LayoutModel.js
@@ -3,8 +3,6 @@ define(['backbone'], function(Backbone) {
     var LayoutModel = Backbone.Model.extend({
         
         defaults: {
-            'row': 0,
-            'col': 0,
             'alignment': 'left'
         }
 
