148f4188f06d855da9d6ec415d4e04fec9292a9d
Karan Sikka <karanssikka@gmail.com>
2014-04-07 16:13:34 -0400

    this isnt changing

--- push_statics.sh
+++ push_statics.sh
@@ -7,5 +7,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 assets=$DIR/appcubator/dist_static/*
 echo "Deploying: $assets"
 
-s3cmd sync --force --reduced-redundancy --acl-public $assets s3://$AWS_STORAGE_BUCKET_NAME/
+s3cmd sync --force --reduced-redundancy --acl-public $assets s3://appcubator-hosting/
 
